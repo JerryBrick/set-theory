440ff5fef7b6c6e4535e802949cd0cea40911948
JerryBrick <ccs81.mr@gmail.com>
2021-06-07 23:24:09 -0500

    Remove lines

--- main.lua
+++ main.lua
@@ -25,62 +25,60 @@ local parsedA = fns.parseSet(A)
 local parsedB = fns.parseSet(B)
 
 print()
-print("---------------------------")
 
 -- Imprimir conjuntos
 print("Conjuntos")
-print("A: " .. fns.stringifySet(parsedA))
-print("B: " .. fns.stringifySet(parsedB))
-print("---------------------------")
+print("  A: " .. fns.stringifySet(parsedA))
+print("  B: " .. fns.stringifySet(parsedB))
+print()
 
 -- Cardinalidad
 print("Cardinalidad")
-print("#A: " .. fns.calculatecardinality(parsedA))
-print("#B: " .. fns.calculatecardinality(parsedB))
-print("---------------------------")
+print("  #A: " .. fns.calculatecardinality(parsedA))
+print("  #B: " .. fns.calculatecardinality(parsedB))
+print()
 
 -- Conjunto potencia
 local powerSetA = fns.calculatePowerSet(parsedA)
 local powerSetB = fns.calculatePowerSet(parsedB)
 print("Conjuntos potencia")
-print("P(A): " .. fns.stringifySet(powerSetA))
-print("P(B): " .. fns.stringifySet(powerSetB))
-print("---------------------------")
+print("  P(A): " .. fns.stringifySet(powerSetA))
+print("\n  P(B): " .. fns.stringifySet(powerSetB))
+print()
 
 -- Cardinalidad del conjunto potencia
 print("Cardinalidad del conjunto potencia")
-print("#P(A): " .. fns.calculatecardinality(powerSetA))
-print("#P(B): " .. fns.calculatecardinality(powerSetB))
-print("---------------------------")
+print("  #P(A): " .. fns.calculatecardinality(powerSetA))
+print("  #P(B): " .. fns.calculatecardinality(powerSetB))
+print()
 
 -- Union
 local union = fns.calculateUnion(parsedA, parsedB)
 print("Union")
-print("AuB: " .. fns.stringifySet(union))
-print("---------------------------")
+print("  AuB: " .. fns.stringifySet(union))
+print()
 
 -- Intersection
 local intersection = fns.calculateIntersection(parsedA, parsedB)
 print("Interseccion")
-print("AnB: " .. fns.stringifySet(intersection))
-print("---------------------------")
+print("  AnB: " .. fns.stringifySet(intersection))
+print()
 
 -- Difference
 print("Diferencia")
-print("A/B: " .. fns.stringifySet(fns.calculateDiff(parsedA, parsedB)))
-print("---------------------------")
+print("  A/B: " .. fns.stringifySet(fns.calculateDiff(parsedA, parsedB)))
+print()
 
 -- Producto cartesiano
 print("Producto cartesiano")
-print("AxB: " .. fns.stringifySet(fns.calculateCartesianProduct(parsedA, parsedB)))
-print("---------------------------")
+print("  AxB: " .. fns.stringifySet(fns.calculateCartesianProduct(parsedA, parsedB)))
+print()
 
 -- Extra
 local powerSetsDifference = fns.calculateUnion(powerSetA, powerSetB)
 print("Extras")
-print("P(A) / P(B): " .. fns.stringifySet(powerSetsDifference))
-print("#(P(A) / P(B)): " .. fns.calculatecardinality(powerSetsDifference))
-print("(AnB)x(AuB): " .. fns.stringifySet(fns.calculateCartesianProduct(intersection, union)))
-print("---------------------------")
+print("  P(A) / P(B): " .. fns.stringifySet(powerSetsDifference))
+print("\n  #(P(A) / P(B)): " .. fns.calculatecardinality(powerSetsDifference))
+print("\n  (AnB)x(AuB): " .. fns.stringifySet(fns.calculateCartesianProduct(intersection, union)))
+print()
 
-print("")
